e51c7082b7418051a8d0f943c7bebd166a9971a8
UDAYKOTESWARARAOCHAKRAPU <udaykoteswarao@gmail.com>
2025-03-16 09:27:20 +0530

    out

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { motion, useScroll } from "framer-motion";
+import { motion } from "framer-motion";
 import "../Css/Home.css";
 import CharacterGrid from "../Componetnts/CharacterGrid";
 
 function Home() {
-  const { scrollYProgress } = useScroll();
-
   return (
     <motion.div
       className="scroll-container"
@@ -13,26 +11,8 @@ function Home() {
       animate={{ opacity: 1 }}
       transition={{ duration: 1 }}
     >
-      {/* Progress Bar */}
-      <motion.div
-        className="progress-bar"
-        style={{
-          scaleX: scrollYProgress,
-          transformOrigin: "left",
-          height: "4px",
-          backgroundColor: "#FFA500", // Naruto theme color
-          position: "fixed",
-          top: 0,
-          left: 0,
-          width: "100%",
-          zIndex: 100
-        }}
-      />
-
       {/* Hero Section */}
       <div className="hero-container">
-        <div className="chakra-effect"></div>
-
         <motion.h1
           className="hero-title"
           initial={{ opacity: 0, y: -50 }}
